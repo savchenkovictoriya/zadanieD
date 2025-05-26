d8822752e6ff2c61f6d282ac286367aa663f7d58
Viktoriia Savchenko <savchenko.victoriya828@gmail.com>
2025-05-26 23:34:05 +0300

    review changes

--- src/main/java/vika/savchenko/Main.java
+++ src/main/java/vika/savchenko/Main.java
@@ -1,10 +1,18 @@
 package vika.savchenko;
 
+import java.util.Random;
+import java.util.random.RandomGenerator;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("sex");
         System.out.println("Dima pidor");
+        int[] array = new int[10];
+        Random random = Random.from(RandomGenerator.getDefault());
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(11);
+        }
     }
 
 
